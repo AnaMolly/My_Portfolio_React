6c290dfcef5320538497fb6d9f36d7fe3481da7b
AnaMolly <analia.mohamdally@gmail.com>
2021-07-22 16:30:12 -0400

    installed dependencies and added navbar functionality

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+//import Project from "./components/Project";
+import Footer from "./components/Footer";
+
+import About from "./components/pages/About";
+import Portfolio from "./components/pages/Portfolio";
+import Resume from "./components/pages/Resume";
+import Contact from "./components/pages/Contact";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Navbar />
+        <Route exact path="/" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+        <Route path="/resume" component={Resume} />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
