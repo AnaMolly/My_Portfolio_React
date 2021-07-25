6729612c79a897b7a5a7f46aebaa71b53d055134
AnaMolly <analia.mohamdally@gmail.com>
2021-07-24 22:43:10 -0400

    added media logos to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import gitimg from '../images/githublogo.png'
+import linkimg from '../images/linkedinlogo.png'
+import twitimg from '../images/twitterlogo.png'
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container text-center mb-5">
+        <div className="footerlogos">
+          <a href="https://github.com/AnaMolly"><img src={gitimg} className="logos" style={{maxWidth:'35px', margin:'10px',opacity:"0.4"}} alt="github logo"></img></a>
+          <a href="https://www.linkedin.com/in/analia-mohamdally-1a7438179/"><img src={linkimg} className="logos" style={{maxWidth:'35px', margin:'10px', opacity:"0.4"}} alt="linkedin logo"></img></a>
+          <a href="https://stackexchange.com/users/22293447/analia"><img src={twitimg} className="logos" style={{maxWidth:'35px', margin:'10px',opacity:"0.4"}} alt="twitter logo"></img></a>
+        </div>
         <p>&copy; 2021 - Made by Analia</p>
       </div>
     </footer>
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,7 @@ function Project(props) {
   return (
     <div className="contentpages">
         <div className="portfoliopieces">
-        <div style={{maxWidth: '400px', margin:'12px'}} >
+        <div style={{maxWidth: '450px', margin:'12px'}} >
             <p className="portpiecetitle">{props.title}</p>
             <a href={props.repo} className="portpiecerepo">View Repository</a>
             <a href={props.hlink} className="portfoliopiece"><img src={props.image} style={{padding:'15px', maxWidth: '100%'}} alt={props.alt}></img> </a>
