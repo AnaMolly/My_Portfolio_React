6b431e6a65f1dbe823bf17cd2c48c0a0ced29fa9
AnaMolly <analia.mohamdally@gmail.com>
2021-07-24 20:42:12 -0400

    added content to porfolio component. styled portfolio page and adjusted about page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,51 @@
 import React from 'react';
+import portimg from '../images/portfolioimg.png'
+import simimg from '../images/simmerimg.png'
+import dayplanimg from '../images/dayplannerimg.png'
+import techimg from '../images/techblogimg.png'
+import weatherimg from '../images/weatherdashboardimg.png'
+import tradeimg from '../images/tradeguideimg.png'
 
 function Portfolio() {
   return (
     <div className="contentpages">
-      <h1>Portfolio</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+        <p style={{paddingBottom:'20px', fontSize:'60px'}}>Portfolio Pieces</p>
+        <p style={{fontStyle:'italic', fontSize:'25px', padding:'30px', color: 'rgb(241, 42, 165)', borderTop:'thin solid lightgray'}}>Click on an image to view the application.</p>
+        <div className="portfoliopieces">
+        <div style={{maxWidth: '450px', margin:'12px'}}>
+            <p className="portpiecetitle">TECH BLOG</p>
+            <a href="https://github.com/AnaMolly/Tech_Blog" className="portpiecerepo">View Repository</a>
+            <a href="https://ancient-earth-72402.herokuapp.com/" className="portfoliopiece"><img src={techimg} style={{padding:'15px', maxWidth: '100%'}} alt="blog"></img> </a>
+        </div>
+        <div style={{maxWidth: '450px', margin:'12px'}}>
+            <p className="portpiecetitle">WEATHER DASHBOARD</p>
+            <a href="https://github.com/AnaMolly/Weather_Dashboard" className="portpiecerepo">View Repository</a>
+            <a href="https://anamolly.github.io/Weather_Dashboard/" className="portfoliopiece"><img src={weatherimg} style={{padding:'15px', maxWidth: '100%'}} alt="weather dashboard"></img></a>
+        </div>
+        <div style={{maxWidth: '450px', margin:'12px'}}>
+            <p className="portpiecetitle">SIMMER COOKBOOK (Collaborative)</p>
+            <a href="https://github.com/AnaMolly/Simmer_Cookbook" className="portpiecerepo">View Repository</a>
+            <a href="https://simmer-cookbook.herokuapp.com/" className="portfoliopiece"> <img src={simimg} style={{padding:'15px', maxWidth: '100%'}} alt="website"></img> </a>
+        </div>
+        <div style={{maxWidth: '450px', margin:'12px'}}>
+            <p className="portpiecetitle">MY PORTFOLIO (HTML & CSS)</p>
+            <a href="https://github.com/AnaMolly/My_Portfolio" className="portpiecerepo">View Repository</a>
+            <a href="https://anamolly.github.io/My_Portfolio/" className="portfoliopiece"> <img src={portimg} style={{padding:'15px', maxWidth: '100%'}} alt="portfolio"></img></a>
+        </div>
+        <div style={{maxWidth: '450px', margin:'12px'}}>
+            <p className="portpiecetitle">TRADE GUIDE (Collaborative)</p>
+            <a href="https://github.com/ErnestAr/Trade_Guide" className="portpiecerepo">View Repository</a>
+           <a href="https://ernestar.github.io/Trade_Guide/" className="portfoliopiece"> <img src={tradeimg} style={{padding:'15px', maxWidth: '100%'}} alt="trade guide"></img></a> 
+        </div>
+        <div style={{maxWidth: '450px', margin:'12px'}}>
+            <p className="portpiecetitle">DAY PLANNER</p>
+            <a href="https://github.com/AnaMolly/Day_Planner" className="portpiecerepo">View Repository</a>
+          <a href="https://anamolly.github.io/Day_Planner/" className="portfoliopiece"> <img src={dayplanimg} style={{padding:'15px', maxWidth: '100%'}} alt="planner"></img></a>  
+        </div>
+      
+       
+        </div>
+       
     </div>
   );
 }
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,7 +12,7 @@ export default function About() {
       <h1 style={{padding:'45px 0px 20px 0px', fontSize: '60px', fontWeight:'200'}}>About Me</h1>
       <img src={me} style={{padding:'45px', maxWidth: '100%'}} alt="girl"></img>
       <p style={{fontSize:'23px', padding:"45px", lineHeight:'2'}}>
-      Hi there! I'm Analia, a full-stack web developer with a background in digital marketing and a keen interest in front-end development. <br></br>Some of my past work can be found under the <strong>Portfolio</strong> tab, and my skillset can be found under the <strong>Resume</strong> tab.  <br></br>If you have any questions, please feel free to contact me!
+      Hi there! I'm Analia, a full-stack web developer with a background in digital marketing and a keen interest in front-end development. <br></br>Some of my past work can be found under the <a href="/portfolio" className="aboutlinks"> Portfolio</a> tab, and my skillset can be found under the <a href="/resume" className="aboutlinks"> Resume</a> tab.  <br></br>If you have any questions, please feel free to contact me!
       </p>
     </div>
   );
